3e9416055d2f2ff6c8202326ed66f49d5b550977
MESSARA <amayas.messara@cgi.com>
2020-07-05 23:48:17 +0200

    :heavy_plus_sign: Add login api

--- LifeProjectBack/settings.py
+++ LifeProjectBack/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'account'
+    'account',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,12 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 AUTH_USER_MODEL = 'account.account'
 
