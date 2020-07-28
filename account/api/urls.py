a97b9ec113a1230b306ab021f9c61d0ad615e938
MESSARA <amayas.messara@cgi.com>
2020-07-28 18:34:24 +0200

    :arrow_up: Adding user id on login response and add api to get user by token

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from account.api.views import registration_view, logout_view
-from rest_framework.authtoken.views import obtain_auth_token
+from account.api.views import registration_view, logout_view, ObtainAuthToken, get_user_by_token_view
 
 
 app_name = 'account'
 
 urlpatterns = [
     path('register', registration_view, name='register'),
-    path('login', obtain_auth_token, name='login'),
-    path('logout/<token>', logout_view, name='logout')
+    path('login', ObtainAuthToken.as_view(), name='login'),
+    path('logout/<token>', logout_view, name='logout'),
+    path('current-user/<token>', get_user_by_token_view, name='get-user')
 ]
--- account/api/views.py
+++ account/api/views.py
@@ -1,7 +1,12 @@
-from rest_framework import status
+from rest_framework import status, parsers, renderers
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.schemas import ManualSchema
+from rest_framework.views import APIView
+from rest_framework.compat import coreapi, coreschema
+
 from account.api.serializers import RegistrationSerializer
 
 
@@ -28,3 +33,49 @@ def logout_view(request, token):
         queryset.delete()
         return Response({'response': 'successfully deleted'})
 
+
+@api_view(['GET'])
+def get_user_by_token_view(request, token):
+    if request.method == 'GET':
+        queryset = Token.objects.get(key=token)
+        return Response({'user_id': queryset.user_id})
+
+
+class ObtainAuthToken(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = AuthTokenSerializer
+    if coreapi is not None and coreschema is not None:
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name="username",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Username",
+                        description="Valid username for authentication",
+                    ),
+                ),
+                coreapi.Field(
+                    name="password",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Password",
+                        description="Valid password for authentication",
+                    ),
+                ),
+            ],
+            encoding="application/json",
+        )
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'id': token.user_id})
